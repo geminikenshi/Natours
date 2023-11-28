02145f4c5e9eb26062ae555457736e9d39240c30
Kenshi <james85529@gmail.com>
2023-11-28 08:27:35 +0800

    modularize code, MVC architecture

---
+++ controllers/tourController.js
@@ -0,0 +1,78 @@
+import fs from 'fs';
+import path from 'path';
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname.slice(1)}/../dev-data/data/tours-simple.json`)
+);
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    result: tours.length,
+    data: { tours },
+  });
+};
+const getTour = (req, res) => {
+  //   console.log(req.params);
+  const id = parseInt(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  //   if (id >= tours.length) {
+  if (!tour) {
+    res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+    return;
+  }
+
+  res.status(200).json({ status: 'success', data: { tour } });
+};
+const createTour = (req, res) => {
+  //   console.log(req.body);
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname.slice(1)}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+const updateTour = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (id >= tours.length) {
+    res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+    return;
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour>',
+    },
+  });
+};
+const deleteTour = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (id >= tours.length) {
+    res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+    return;
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+export { getAllTours, getTour, createTour, updateTour, deleteTour };
---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const getAllUsers = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'Not implemented' });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'Not implemented' });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'Not implemented' });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'Not implemented' });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'Not implemented' });
+};
+
+export { getAllUsers, createUser, getUser, updateUser, deleteUser };
